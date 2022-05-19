8908a5158482011589b2f29afa48507c9cb982d7
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-19 16:35:17 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    todos: [
+      { text: "Comprare il pane", showEdit: false, done: "", hide: "" },
+      { text: "Andare a prendere le lenti a contatto", showEdit: false, done: "", hide: "" },
+      { text: "Farsi la doccia", showEdit: false, done: "", hide: "" },
+    ],
+    newTodo: "",
+    show: false,
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) return;
+      this.todos.push({ text: this.newTodo });
+      this.newTodo = "";
+    },
+
+    editTodo(todo, event) {
+      todo.text = event.target.value;
+      this.toggleEditInput(todo);
+    },
+
+    deleteTodo(todo) {
+      this.todos = this.todos.filter((t) => todo !== t);
+    },
+
+    completeTodo(todo) {
+      todo.done = !todo.done ? "done" : "";
+    },
+
+    toggleEditInput(todo) {
+      todo.showEdit = todo.showEdit ? false : true;
+    },
+
+    toggleCompletedTodos() {
+      this.todos
+        .filter((todo) => todo.done.length > 0)
+        .forEach((todo) => {
+          if (todo.hide.includes("hidden")) {
+            todo.hide = todo.hide.replace("hidden", "");
+          } else {
+            todo.hide += " hidden";
+          }
+        });
+    },
+  },
+});
